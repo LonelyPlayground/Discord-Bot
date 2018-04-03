d050bfed756f5925f090f0abe95e88cb64204bce
Erik Mead <emead724@gmail.com>
2018-04-03 13:34:13 +0000

    working on roll

--- Bot/Gambling/gambling.py
+++ Bot/Gambling/gambling.py
@@ -1,1 +1,6 @@
-#omment
+class Gamble:
+
+    def roll(minimum, maximum):
+        if not (isinstance(minimum, int) and isinstance(maximum, int))
+            return "Must roll between two whole numbers ducko"
+    def trivia():
