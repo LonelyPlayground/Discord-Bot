7d673cfbce7ff8761ecacba863b5e155cb174476
Erik Mead <emead724@gmail.com>
2018-04-13 12:40:37 +0000

    working on join now

--- Bot/Gambling/gambling.py
+++ Bot/Gambling/gambling.py
@@ -1,8 +1,10 @@
 from random import *
 class Gamble:
 
+    gamblingSession = False
+
     #users can roll between numbers
-    def roll(self, args):
+    def roll(self, args, user):
         minimum = 0
         maximum = 0
         #no args default to 1-100 elsewise complain about missing/too many args
@@ -13,15 +15,25 @@ class Gamble:
 	        minimum = args[0]
 	        maximum = args[1]
         else:
-            return "Usage: #roll <minimum whole number> <maximum whole number>"
+            return "<@{}> Give me some input here geez or none, that's fine too. #roll <minimum> <maximum>".format(user)
         #try to convert to int, if not a number yell at them
         try:
             minimum = int(minimum)
             maximum = int(maximum)
         except:
-            return "Usage: #roll <minimum whole number> <maximum whole number>"
+            return "<@{}> Really, that's what you want me to roll between? No. #roll <minimum> <maximum>".format(user)
         #make sure the minimum is smaller
         if minimum >= maximum:
-            return "Usage: #roll <minimum whole number> <maximum whole number>"
+            return "<@{}> Minimum<Maximum go back to elementary school. #roll <minimum> <maximum>".format(user)
         #and roll!
-        return "Roll between {} and {}, you rolled: {}".format(minimum, maximum, randint(minimum, maximum))
+        return "Roll between {} and {}, <@{}> rolled: {}".format(minimum, maximum, user, randint(minimum, maximum))
+
+    def join(self):
+        pass
+
+    #gambling function administrates all the other functions
+    def gamble_init(self):
+        global gamblingSession
+        gamblingSession = True
+        join()
+
--- Bot/MyBot.py
+++ Bot/MyBot.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 from Token.Token import *
 from Erik_Quotes.quote import *
-from Gambling.gambling import *
+from gambling.gambling import *
 #API refrence sheet - http://discordpy.readthedocs.io/en/latest/api.html#client
 #bot commands framework https://discordpy.readthedocs.io/en/rewrite/ext/commands/commands.html
 Client = discord.Client()
@@ -40,11 +40,15 @@ async def add_quote(ctx):
 #users roll between set amounts
 @client.command(pass_context = True)
 async def roll(ctx, *args):
-	roll = house.roll(args)
+	roll = house.roll(args, ctx.message.author.id)
 	await client.say(roll)
 #TODO have a data base of users and a balence, then allow them to gamble by making a session and having them roll, then having the lowest roll pay the difference to the highest roll.
 @client.command(pass_context = True)
-async def gamble(ctx):
+async def gamble(ctx, *args):
+	house.gambling_init
+	await client.say("TODO, Contact @Blast38#9189 or other contributers and make him get to work!")
+@client.command(pass_context = True)
+async def stop_join(ctx):
 	await client.say("TODO, Contact @Blast38#9189 or other contributers and make him get to work!")
 #runs client
 client.run(token)
